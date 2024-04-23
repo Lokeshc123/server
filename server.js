e3a4681e38d368bb4e97fe76cb40f5222168f585
Lokesh Chauhan <68843786+Lokeshc123@users.noreply.github.com>
2024-04-23 10:32:23 +0530

    backend

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDb = require("./config/database");
+const cloudinary = require("cloudinary");
+//Configure dotenv to use the .env file
+dotenv.config({ path: "server/config/.env" });
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+//Connect to the database
+connectDb();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
